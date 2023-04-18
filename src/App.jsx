a2bd13df5d13131cac4c9f9331ae67cd4dd063e4
kate <kate@okrasolar.com>
2023-04-18 15:40:20 +0800

    feat: Setup files and initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Grommet } from 'grommet'
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <Grommet theme={theme} full>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+    </Grommet>
+  )
+}
+
+export default App
